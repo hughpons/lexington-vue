1b21936d398a63ead540e30a85eaec902abb300c
XL Zhang <hughpons@yahoo.com>
2025-04-15 21:06:54 -0400

    Add some settings to the registry using powershell.

---
+++ Source/RegAppSettings.ps1
@@ -0,0 +1,28 @@
+﻿#Set-ExecutionPolicy Unrestricted -Scope CurrentUser
+
+function SetKeyValue {
+    param (
+        [parameter(Mandatory=$true)]$Path,
+        [parameter(Mandatory=$true)]$Name,
+        [parameter(Mandatory=$true)]$Value
+    )
+    
+    if (Test-Path -Path $Path) {
+        Write-Output "$Path exists."
+    }
+    else {
+        New-Item -Path $Path -Force | Out-Null
+    }
+
+    $r = (Get-Item -Path $Path -EA Ignore).Property -contains $Name
+    if ($r) {
+        Set-ItemProperty -Path $Path -Name $Name -Value $Value -Force
+    } else {
+        New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType String -Force
+    }
+}
+
+# Add database connection settings to the registry.
+$ServerKey = "HKLM:\Software\LexingtonAcademy\Server"
+SetKeyValue $ServerKey InstanceName local | Out-Null
+SetKeyValue $ServerKey DatabaseName SchoolDB | Out-Null
